fc154ed2d43694f9634ce20fe78d9d37d91b651c
Gonzalo Nahuel Vaca <vacagonzalo@gmail.com>
2023-03-24 19:27:06 -0300

    read until end of file

--- ch04/ctee/ctee.c
+++ ch04/ctee/ctee.c
@@ -2,7 +2,6 @@
  * @brief Clone of tee (exercise 4-1)
  */
 
-#include <asm-generic/errno-base.h>
 #include <fcntl.h>
 #include <unistd.h>
 
@@ -12,29 +11,21 @@
 
 int main(int argc, char **argv) {
   if(argc < 2){
-    return -EPERM;
+    return -1;
   }
-  char buffer[BUFFER_SIZE] = {0};
-
-  int numr = read(STDIN_FILENO, (void *)buffer, BUFFER_SIZE);
-  if (numr < 0) {
-    return -EIO;
-  }
-
   int open_flags = O_CREAT | O_WRONLY | O_TRUNC;
   int file_perm = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH;
   int fd = open(argv[1], open_flags, file_perm);
   if (fd < 0) {
-    return -ENOENT;
+    return -1;
   }
-  int numd = write(fd, (const void *)buffer, numr);
-  if (numd < 0) {
-    return -EIO;
-  }
+  char buffer[BUFFER_SIZE] = {0};
 
-  int numw = write(STDOUT_FILENO, (const void *)buffer, numr);
-  if (numw < 0) {
-    return -EIO;
+  int numr = 0;
+  while((numr = read(STDIN_FILENO, (void *)buffer, BUFFER_SIZE)) > 0)
+  {
+    write(fd, (const void *)buffer, numr);
+    write(STDOUT_FILENO, (const void *)buffer, numr);
   }
 
   return 0;
